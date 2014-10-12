22dedb0abd9d05b312d6a70f7f733f695280bfd8
jonas <jonasbru@wanadoo.fr>
2014-10-12 22:21:34 +0200

    nbproject folder added, make doesn't work otherwise

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=lib2DEngine.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/lib2DEngine.so
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=lib2DEngine.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/lib2DEngine.so.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=2dengine
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/2dengine
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=2dengine.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/2dengine.tar
